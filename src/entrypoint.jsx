21728df4fa5c752e7455842e430c3b88ea85630c
Dmitrii Kopeikin <kopeikindp@gmail.com>
2023-06-30 19:36:49 +0600

    Webpack configured.

--- src/entrypoint.jsx
+++ src/entrypoint.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-// import App from './App';
+import App from './App.jsx';
 
 const rootElement = document.getElementById('react-root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <p>Hello World, React!</p>
+    <App />
   </StrictMode>
 );
